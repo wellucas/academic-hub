53d9004e0e660bedd642e13db625e34b4ca855a9
Wellison Soares <wellison211207@gmail.com>
2026-06-10 19:56:16 -0300

    criação do index.css, menu.js e mudanças no html

---
+++ script/menu.js
@@ -0,0 +1,8 @@
+function toggleMenu()
+{
+    const actions = document.querySelector("#header-actions");
+    const menu = document.querySelector("#btn-menu");
+
+    actions.classList.toggle("active");
+
+}
--- script/script.js
+++ script/script.js
@@ -1,14 +1,20 @@
-const cards = document.querySelectorAll(".flashcard-item");
+import { BASE_CARDS } from "./resource.js";
 
 const searchCards = (query) => {
   const flashcardsGrid = document.getElementById("flashcards-grid");
 
-  const filteredCards = Array.from(cards).filter((card) => {
-    const question = card.querySelector(".question").textContent.toLowerCase();
+  const filteredCards = BASE_CARDS.filter((card) => {
+    const question = card.pergunta.toLowerCase();
     return question.includes(query.toLowerCase());
   });
 
   flashcardsGrid.innerHTML = filteredCards
-    .map((card) => card.outerHTML)
+    .map((card) => `
+      <article class="flashcard-item" data-id="${card.id}">
+        <span class="category">${card.categoria}</span>
+        <h2 class="question">${card.pergunta}</h2>
+        <p class="answer-preview">${card.resposta}</p>
+      </article>
+    `)
     .join("");
 };
